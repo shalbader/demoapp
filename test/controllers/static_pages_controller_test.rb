95f1223e68adb00b73c709574aa3334d14604396
shalbader <s.albader@berkeley.edu>
2015-02-20 04:21:52 +0000

    done for the 5th class

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "h1", "Demoapp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "h1", "Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_response "h1", "About"
   end
 
 end
